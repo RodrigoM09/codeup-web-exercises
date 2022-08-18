5f52bd9b17822ed0604c8e3e7bc1dc69f49c5247
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-08-18 14:17:54 -0500

    end of loops, beginning of arrays

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -2,6 +2,8 @@ let oddNumber;
 while(true){
      oddNumber = prompt("Enter an odd number between 1 and 50");
     if (oddNumber <=50 && oddNumber >= 1 && oddNumber % 2 === 1){
+        console.log("Number to skip is: " + oddNumber);
+        console.log(" ");
         break;
     }
 }
