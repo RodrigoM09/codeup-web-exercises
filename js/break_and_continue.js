983e8911e8603e8ddc2a584db576fc3cec256fd9
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-08-18 11:39:02 -0500

    break and continue

---
+++ js/break_and_continue.js
@@ -0,0 +1,14 @@
+let oddNumber;
+while(true){
+     oddNumber = prompt("Enter an odd number between 1 and 50");
+    if (oddNumber <=50 && oddNumber >= 1 && oddNumber % 2 === 1){
+        break;
+    }
+}
+for(let i = 1; i <= 50; i+=2){
+    if (oddNumber == i){
+        console.log("Yikes! Skipping number: " + i);
+        continue;
+    }
+    console.log("Here is an odd number " + i);
+}
--- js/for_loops.js
+++ js/for_loops.js
@@ -1,48 +1,59 @@
-
-        const showMultiplicationTable = parseInt(prompt('Enter an number: '));
-        //creating a multiplication table
-        for(let i = 1; i <= 10; i++) {
-            // multiply i with number
-            const result = i * showMultiplicationTable;
-            // display the result
-            console.log(`${showMultiplicationTable} * ${i} = ${result}`);
-        }
-
-
-
-    for (let i = 0; i < 10; i++){
-        const min = 20;
-        const max = 200;
-        const number = Math.floor(Math.random() * (max - min + 1)) + 1;
-        if (number % 2 === 0){
-            console.log(number + " is even");
-        } else if (number !== 0) {
-            console.log(number + " is odd");
-
-        }
-    }
-
-
-    for(let i = 1; i <= 9; i++){
-        let row =[]
-        for (let a = 1; a <= i; a++) {
-            row.push(i);
-        }
-        console.log(row.join(""));
-    }
-    // same as above, shorter way.
-    // for(let i = 1; i <= 9; i++){
-    //     console.log(i.toString().repeat(i));
+    //
+    //     const showMultiplicationTable = parseInt(prompt('Enter an number: '));
+    //     //creating a multiplication table
+    //     for(let i = 1; i <= 10; i++) {
+    //         // multiply i with number
+    //         const result = i * showMultiplicationTable;
+    //         // display the result
+    //         console.log(`${showMultiplicationTable} * ${i} = ${result}`);
+    //     }
+    //
+    //
+    //
+    // for (let i = 0; i < 10; i++){
+    //     const min = 20;
+    //     const max = 200;
+    //     const number = Math.floor(Math.random() * (max - min + 1)) + 1;
+    //     if (number % 2 === 0){
+    //         console.log(number + " is even");
+    //     } else if (number !== 0) {
+    //         console.log(number + " is odd");
+    //
+    //     }
     // }
-        // Same as above
-        // for(let i = 1; i <= 9; i++) {
-        //     let outputString = "";
-        //     for (let count = 0, count <i; count++){
-        //         outputString = outputString + i;
-        //     }
-        //     console.log(outputString);
-        // }
-
-    for (let i = 100; i >= 5; i -= 5){
-        console.log(i);
+    //
+    //
+    // for(let i = 1; i <= 9; i++){
+    //     let row =[]
+    //     for (let a = 1; a <= i; a++) {
+    //         row.push(i);
+    //     }
+    //     console.log(row.join(""));
+    // }
+    // // same as above, shorter way.
+    // // for(let i = 1; i <= 9; i++){
+    // //     console.log(i.toString().repeat(i));
+    // // }
+    //     // Same as above
+    //     // for(let i = 1; i <= 9; i++) {
+    //     //     let outputString = "";
+    //     //     for (let count = 0, count <i; count++){
+    //     //         outputString = outputString + i;
+    //     //     }
+    //     //     console.log(outputString);
+    //     // }
+    //
+    // for (let i = 100; i >= 5; i -= 5){
+    //     console.log(i);
+    // }
+    //
+    let string = "";
+    for (let i = 0; i < 8; i++){
+        let word = prompt("let's make a sentence! Enter a word: ");
+        if (word === "curses"){
+            console.log("That word is not allowed");
+            continue;
+        }
+        string += " " + word;
+        console.log("Your sentence is: " + string);
     }
