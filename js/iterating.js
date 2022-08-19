bc06818580b7ceafb518ba7a86f05cd27d9de148
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-08-18 21:28:09 -0500

    iterating

--- js/iterating.js
+++ js/iterating.js
@@ -1,34 +1,41 @@
-(function(){
-    "use strict";
+ "use strict";
 
     /**
      * TODO:
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    let names = ['John', 'Ruth', 'David', 'Samuel']
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
-     */
+     */ console.log(names.length)
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+        console.log(names[0]);
+        console.log(names[1]);
+        console.log(names[2]);
+        console.log(names[3]);
+        console.log(names[4]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+        for (let i = 0; i < names.length; i++){
+            console.log(names[i]);
+        }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+        names.forEach(element => console.log(element));
 
     /**
      * TODO:
@@ -43,5 +50,17 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+        let numbersArray = ['1','2','3','4','5']
+        const firstElement = numbersArray[0];
+        const second = numbersArray[1];
+        const last = numbersArray[4]
+        console.log(firstElement)
+        console.log(second)
+        console.log(last);
+ for( let i = 0; i < numbersArray.length -3; i++ ) {
+     if (i === numbersArray.length - 1) {
+     } else {
+         console.log(numbersArray[i]);
+     }
+ }
 
-})();
