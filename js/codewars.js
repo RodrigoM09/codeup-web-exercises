ab043f9205ca1469b5f3fcebbe8fa66413f48689
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-09-02 16:41:41 -0500

    codewars

--- js/codewars.js
+++ js/codewars.js
@@ -5,20 +5,34 @@ let shoes = [
     [0, 23]
 ];
 
-function pairOfShoes(shoes){
-    let shoeThatMatches;
-    let shoeHasMatch = false;
-    let newArray;
-    for (let i = 1; i < shoes.length; i++){
-        if (shoes[i][1] === shoes[0][1]){
-            if(shoes[i][0] !== shoes[0][0]) {
-                shoeHasMatch = true;
-                shoeThatMatches = true;
-                newArray =shoes.slice(1, shoes.length);
+// function pairOfShoes(shoes) {
+//         let shoeHasMatch = false;
+//         let shoeThatMatches;
+//         let newArray;
+//         for (let i = 1; i < shoes.length; i++) {
+//             if (shoes[i][1] === shoes[0][1]) {
+//                 if (shoes[i][0] !== shoes[0][0]) {
+//                     shoeHasMatch = true;
+//                     shoes.splice(i, 1);
+//                     shoes.splice(0, 1);
+//                     return pairOfShoes(shoes);
+//                 }
+//             }
+//         }
+//         return shoeHasMatch;
+// }
 
-            }
-        }
-    }
+function pairOfShoes(shoes) {
 
-    return shoeHasMatch;
+        function findAMatch(shoesArray) {
+            for (let i = 1; i < shoesArray.length; i++) {
+                if (shoesArray[i][1] === shoesArray[0][1]) {
+                    if (shoesArray[i][0] !== shoesArray[0][0]) {
+                        shoesArray.splice(i, 1);
+                        shoesArray.splice(0, 1);
+                        return true;
+                    }
+                }
+            }return shoes.length < 2 || !shoeHasMatch ? shoeHasMatch : pairOfShoes(shoes);
+
 }
