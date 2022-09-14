dd4a055f922c398fe035f9f1f54a99dcdd0c10aa
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-09-14 15:08:56 -0500

    bom object model

--- js/browser-object-model.js
+++ js/browser-object-model.js
@@ -1,11 +1,33 @@
-let sayHey = function(){
-    let count = 0;
+// let sayHey = function(){
+//     let count = 0;
+//
+//     function hey(){
+//         if (count < 10) {
+//             console.log('hey' + count);
+//             count++;
+//         } else {
+//             clearInterval(theInterval);
+//         }
+//     }
+//    let theInterval = setInterval(hey, 1000);
+// }
 
-    function hey(){
-        console.log('hey' + count);
-        count++;
+
+let count = 0;
+let thisTimeItWillStop;
+
+function hey(){
+    if (count < 10){
+        console.log("hey " + count);
+        count++
+    }else{
+        clearInterval(thisTimeItWillStop);
     }
-
-    setInterval(hey, 1000);
 }
+    thisTimeItWillStop = setInterval(hey, 1000);
 
+setTimeout(wakeUpUser, 5000);
+
+function wakeUpUser(){
+    alert("Wake UUUUUPPPP!!!!");
+}
