1d2b35eec3c3504b8d5871dff3b89c7166ac0ce9
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2022-09-14 16:29:51 -0500

    DOM

--- document-object-model.js
+++ document-object-model.js
@@ -1,3 +1,16 @@
 let accessTheDom = document.getElementById("dc");
 let showMe = accessTheDom.innerHTML;
-alert(showMe);
+console.log(showMe);
+
+let marvel = document.getElementById("marvel");
+let preference = "dc";
+
+let like = document.getElementById("like");
+let dontLike = document.getElementById("dontLike");
+let output = `${dontLike.innerHTML} ${document.getElementById(preference).innerHTML} movies`;
+console.log(output);
+
+
+accessTheDom.innerHTML += ' Animated Universe';
+
+marvel.setAttribute('class', 'hotPink')
